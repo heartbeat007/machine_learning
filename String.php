4b81914de753939ec7f65f2aa0c6165a366eba1f
rebirth <ornobtanvir@gmail.com>
2018-03-08 13:35:08 +0530

    this is a string test

---
+++ String.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rebirth
+ * Date: 8/3/18
+ * Time: 1:17 PM
+ * subject:String
+ **/
+
+
+echo "hello world <br />"; //string
+echo 'Hello world <br />';//another string
+
+//saving striong to variale
+$string1 = "my name is tanvir and now i am learning php <br />";
+$string2 = "hello ";
+$string3 = "world";
+echo $string2;
+echo $string3;
+$phrase = $string2 . " " .$string3; //its more like a string concatenation
+
+echo "$phrase again<br />"; //it is only possible in double quote
+echo "{$phrase}again<br />";
+
+
+
+
+
+
+
+?>
