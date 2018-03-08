484a9bbc2898e9b4020ff497cb16fb25456978fa
rebirth <ornobtanvir@gmail.com>
2018-03-08 13:11:38 +0530

    this is a test

---
+++ variable first.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rebirth
+ * Date: 8/3/18
+ * Time: 12:46 PM
+ * subject:php learning(variables)
+**/
+
+//using different types of variable
+//variables
+$variable1 = 10;
+$variable2 = "hello world";
+$variable3 = 10.2;
+echo $variable1;
+echo $variable2;
+echo $variable3;
+
+
+
+
+
+?>
