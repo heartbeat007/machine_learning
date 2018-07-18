6f72d56f4df3511e0c7ef60cf7bb01fccf5ff9b3
heartbeat007 <36996161+heartbeat007@users.noreply.github.com>
2018-07-18 20:29:46 +0530

    Add files via upload
    this is a crud app

---
+++ crudapp/database.php
@@ -0,0 +1,19 @@
+<?
+//in this script we gonna just 
+//open a database connection for the further use
+$host = 'secret';
+$username='secret';
+$password='secret';
+$dbname = "secret";
+$conn = mysqli_connect($host,$username,$password,$dbname);
+//now we crate a connection so if there is 
+//no connection we send a confirmation msg
+if (!$conn) {
+	die("connection failed ".mysqli_errno($conn));
+}
+else {
+	//echo "successful";
+}
+
+
+?>
